a5bddcc5bbbc0f3504005cbce14399e9b392b794
csvitor-dev <csvitor.dev@gmail.com>
2024-08-25 19:17:52 -0300

    feat(file manipulation): adding layer to application to access files

--- parser/PropositionalFormula.py
+++ parser/PropositionalFormula.py
@@ -3,15 +3,19 @@ from parser.SubformulaExtractor import SubformulaExtractor
 
 class PropositionalFormula:
     @staticmethod
-    def _get_parsed_formula(formula):
+    def _get_parsed_formula(formula: str):
         try:
           parse_tree = parser.parse(formula)
         except:
           return
         return parse_tree
+    
+    @staticmethod
+    def formula_is_valid(formula: str) -> bool:
+       return PropositionalFormula._get_parsed_formula(formula) != None
 
     @staticmethod
-    def get_main_conective_and_immediate_subformulas(formula):
+    def get_main_conective_and_immediate_subformulas(formula: str):
       parse_tree = PropositionalFormula._get_parsed_formula(formula)
       if parse_tree is None:
         return None, None
