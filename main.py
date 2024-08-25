f40756946b84bd112208dbfada8b059c1534f60b
csvitor-dev <csvitor.dev@gmail.com>
2024-08-25 19:22:28 -0300

    fix: remodeling modules definition

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from parser.PropositionalFormula import PropositionalFormula
+import sys
+# from parser.PropositionalFormula import PropositionalFormula
+from file_manipulation.FileManager import FileManager
 
-#EXEMPLO DE USO
+sys.path.append('..\\pyTableaux\\')
+
+test = FileManager.get_formulas_on_file("example.tab")
+
+print(test)
+
+""" #EXEMPLO DE USO
 conective, subformulas = PropositionalFormula.get_main_conective_and_immediate_subformulas("(p->(q->p))")
 if conective is not None:
   print("valida")
   print(conective, subformulas)
 else:
-  print("malformulada")
+  print("malformulada") """
