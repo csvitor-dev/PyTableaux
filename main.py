d6c04a3911481788010a2fad81f3fbb4e420fe6b
csvitor-dev <csvitor.dev@gmail.com>
2024-08-27 09:41:09 -0300

    feat: creating entrypoint on 'main.py' file

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+""" 
+
+"""
+
 import sys
-# from parser.PropositionalFormula import PropositionalFormula
-from file_manipulation.FileManager import FileManager
+from file_manipulation.FileManager import FileManager as FM
 
 sys.path.append('..\\pyTableaux\\')
 
-test = FileManager.get_formulas_on_file("example.tab")
+def main(args) -> None:
+    print(args)
+    """ build = FM()
 
-print(test)
+    try:
+        result = build.get_formulas_on_file()
+    except FileNotFoundError as e:
+        print(e.args[0])
+        return
+    print(result) """
 
-""" #EXEMPLO DE USO
-conective, subformulas = PropositionalFormula.get_main_conective_and_immediate_subformulas("(p->(q->p))")
-if conective is not None:
-  print("valida")
-  print(conective, subformulas)
-else:
-  print("malformulada") """
+if __name__ == "__main__":
+    main(sys.args)
