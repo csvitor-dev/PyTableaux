dd99ddb54e0b5f0c55a5b17bff7d07e9eddf7ad6
csvitor-dev <csvitor.dev@gmail.com>
2024-08-29 09:59:44 -0300

    fix: aestherics

--- file_manipulation/FileManagerContent.py
+++ file_manipulation/FileManagerContent.py
@@ -1,16 +1,17 @@
-from solver.FormulaBuilder import FormulaBuilder as FB
+from _types.FormulaBuilder import FormulaBuilder as FB
+from _types.Formula import Formula
 
 class FileContentManager: 
 
     def __init__(self, raw_file_content: list[str]) -> None:
         self.__file_content: list[str] = self.__clear_content(raw_file_content)
 
-    def __clear_content(self, arg: list[str]):
+    def __clear_content(self, arg: list[str]) -> list[str]:
         for index, line in enumerate(arg):
             arg[index] = line.replace('\n', '')
         return arg
 
-    def get_formulas_on_file(self) -> dict[str: bool] | None:
+    def get_formulas_on_file(self) -> Formula | None:
         number_formulas = int(self.__file_content[0])
         formulas = self.__file_content[1:]
         return FB.build_marked_formulas(number_formulas, formulas)
--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 """ 
     TRABALHO DE LÓGICA PARA COMPUTAÇÃO
 Equipe:
-    - Nicolas
-    - Vitor Costa de Sousa (536678)[ES]
+    - Nicolas Maurício Chaves (539802) [ES]
+    - Vitor Costa de Sousa    (536678) [ES]
 """
 
 import sys
 from file_manipulation.FileContentValidator import FileContentValidator as FCV
 from file_manipulation.FileManagerContent import FileContentManager
+from solver.Tableaux import Tableaux
 
 def main() -> None:
     raw: list[str] = sys.stdin.readlines()
@@ -22,6 +23,9 @@ def main() -> None:
     
     if formulas is None:
         print("the formulas were not generated")
+
+    tableaux = Tableaux(formulas)
+    tableaux.solve()
 
 if __name__ == "__main__":
     main()
