b2f21cc1c226efcec95dc04d18a4bbc4cf672026
csvitor-dev <csvitor.dev@gmail.com>
2024-08-30 10:57:58 -0300

    feat: structuring the branch tree

--- solver/RulesExpander.py
+++ solver/RulesExpander.py
@@ -1,7 +1,21 @@
+from _types.tokens import TOKEN_AND, TOKEN_IMPL, TOKEN_NEG, TOKEN_OR
+
 class RulesExpander:
     
     def __init__(self) -> None:
-        self.alpha_rules = []
-        self.beta_rules = []
+        self.__alphas = {TOKEN_AND: True, TOKEN_OR: False, TOKEN_IMPL: False}
+        self.__betas = {TOKEN_AND: False, TOKEN_OR: True, TOKEN_IMPL: True}
 
+    def expand(self, marking: bool, operator: str, subformulas: list[str]) -> list[tuple[str, bool]]:
+        if operator == TOKEN_NEG:
+            return self.__reversing_polarity(marking, subformulas[0])
+        ...
+
+    def __reversing_polarity(self, marking: bool, formula: str) -> list[tuple[str, bool]]:
+        return [(formula, not marking)]
+
+    def __alpha_rule(self):
+        ...
     
+    def __beta_rule(self):
+        ...
--- solver/Tableaux.py
+++ solver/Tableaux.py
@@ -5,10 +5,12 @@ from _types.FormulaCollection import FormulaCollection
 
 class Tableaux:
     def __init__(self, formulas: FormulaCollection) -> None:
-        self.__formulas = formulas
+        self.__atomic_formulas_list = []
         self.__branchs = BranchsTree(formulas)
+        self.__formulas = formulas
         self.__rules_agent = RulesExpander()
 
     def solve(self) -> None:
         for formula in self.__formulas.get_formulas():
             print(PF.get_main_conective_and_immediate_subformulas(formula))
+    
